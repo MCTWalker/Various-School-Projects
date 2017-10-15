5f890a090e09287cb234bb1c2b4af4f0459b8ca1
MCTWalker <MCTWalker@users.noreply.github.com>
2017-10-14 21:09:14 -0600

    added rules

--- RosiesRose.pl
+++ RosiesRose.pl
@@ -1,7 +1,3 @@
-all_different([H | T]) :- member(H, T), !, fail.
-all_different([_ | T]) :- all_different(T).
-all_different([_]).
-
 item(Balloons).
 item(Candles).
 item(Chocolates).
@@ -42,4 +38,22 @@ solve :-
 				[Ida, IdaItem, IdaEvent, IdaRose],
                 [Leroy, LeroyItem, LeroyEvent, LeroyRose] ],
 				
+	member([Jeremy, _, SeniorProm, _], Quartets),
 	\+ member([Stella, _, Wedding, _], Quartets),
+	member([Stella, _, _, CottageBeauty], Quartets),
+	member([Hugh, _, _, PinkParadise], Quartets),
+	\+ member([Hugh, _, CharityAuction, _], Quartets),
+	\+ member([Hugh, _, Wedding, _], Quartets),
+	member([_, Streamers, Anniversary, _], Quartets),
+	member([_, Balloons, Wedding, _], Quartets),
+	member([_, Chocolates, _, SweetDreams], Quartets),
+	\+ member([Jeremy, _, _, MountainBloom], Quartets),
+	member([Leroy, _, Retirement, _], Quartets),
+	member([_, candles, SeniorProm, _], Quartets),
+	
+	
+	
+all_different([H | T]) :- member(H, T), !, fail.
+all_different([_ | T]) :- all_different(T).
+all_different([_]).
+
